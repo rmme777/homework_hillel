f2252227b713c327561b1ddb2edac3997df4210d
valera <valera.hurricane@gmail.com>
2025-03-23 14:51:36 +0200

    сделал дз 8.1

---
+++ дз8.1.py
@@ -0,0 +1,19 @@
+def add_one(some_list):
+    to_convert = ""
+    new_list = []
+    for i in some_list:
+        to_convert += str(i)
+    int_to_convert = int(to_convert)
+    int_to_convert += 1
+    for j in str(int_to_convert):
+        new_list.append(int(j))
+    return new_list
+
+
+
+assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
+assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
+assert add_one([0]) == [1], 'Test3'
+assert add_one([9]) == [1, 0], 'Test4'
+print("ОК")
+
